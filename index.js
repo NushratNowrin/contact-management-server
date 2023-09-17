7fccc94b88a750d0b12c1c00693640469d7892a3
NushratNowrin <nowrin2023@gmail.com>
2023-09-18 03:31:41 +0600

    backend update done

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000
 app.use(express.json());
 app.use(cors());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r1co4vf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +22,60 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const database = client.db("contactsDB");
+    const ContactsCollection = database.collection("contacts");
+
+    // get the contacts data from DB
+    app.get('/api/contacts',async(req, res) => {
+        const cursor = ContactsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // Post to the contacts
+    app.post('/api/contacts', async(req, res) => {
+        const toy= req.body;
+        console.log(toy);
+        const result = await ContactsCollection.insertOne(toy);
+        res.send(result);
+    })
+
+    // Delete one contact
+    app.delete('/api/contacts/:id', async(req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await ContactsCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    // Update contact
+    app.put('/api/contacts/:id', async(req, res) =>{
+        const id = req.params.id;
+        const contact = req.body;
+        console.log(id, contact);
+        const filter = {_id: new ObjectId(id)}
+        const options = {upsert: true}
+        const updatedUser = {
+            $set: {
+                userName: contact.userName ,
+                number: contact.number,
+                email: contact.email
+            }
+        }
+        const result = await ContactsCollection.updateOne(filter, updatedUser, options);
+        res.send(result)
+    })
+
+    // get single contact by id
+    app.get('/api/contacts/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const contact = await ContactsCollection.findOne(query);
+        res.send(contact);
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
